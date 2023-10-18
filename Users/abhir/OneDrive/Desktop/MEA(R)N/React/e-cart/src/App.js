ac08197a363af278a08b836bea1ff6e04da67483
AbhiramiKarthikeyan <abhiramikarthikeyan29@gmail.com>
2023-10-18 13:09:50 +0530

    e-cart

---
+++ Users/abhir/OneDrive/Desktop/MEA(R)N/React/e-cart/src/App.js
@@ -0,0 +1,25 @@
+
+import { Routes,Route } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+import Wishlist from './Pages/Wishlist';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
+
+function App() {
+  return (
+   <>
+   <Header/>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/cart' element={<Cart/>}/>
+    <Route path='/wishlist' element={<Wishlist/>}/>
+   </Routes>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
